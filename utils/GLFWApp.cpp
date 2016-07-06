ac77883be284be0455493c6e02639ace5a3cd4bc
Corentin Wallez <corentin@wallez.net>
2016-07-06 13:35:41 -0700

    Fix for core profile

--- utils/GLFWApp.cpp
+++ utils/GLFWApp.cpp
@@ -12,6 +12,23 @@ namespace {
     void APIENTRY OnKHRDebug(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
         std::cerr << "KHR_debug says: " << message << std::endl;
     }
+
+    bool ExtensionAvailable(const GLFWApp::InitParams& params, const std::string& extension) {
+        if (params.coreProfile) {
+            GLint nExtensions = 0;
+            glGetIntegerv(GL_NUM_EXTENSIONS, &nExtensions);
+            for (GLint i = 0; i < nExtensions; i++) {
+                if (std::string(reinterpret_cast<const char*>(glGetStringi(GL_EXTENSIONS, i))) == extension) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            std::string extensions = reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS));
+            return extensions.find("GL_KHR_debug") != std::string::npos;
+        }
+    }
+
 }
 
 int GLFWApp::Run(GLFWApp* app_) {
@@ -48,8 +65,7 @@ int GLFWApp::Run(GLFWApp* app_, const InitParams& params) {
         return 1;
     }
 
-    std::string extensions = reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS));
-    if (extensions.find("GL_KHR_debug") != std::string::npos) {
+    if (ExtensionAvailable(params, "GL_KHR_debug")) {
         glEnable(GL_DEBUG_OUTPUT);
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
         glDebugMessageCallback(OnKHRDebug, nullptr);
